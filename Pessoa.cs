e294cfc1e7be6b749cc1f41ab149e29eaeb84535
Annyanjo <anelliz34dosanjosmelloesilva@gmail.com>
2024-04-02 16:27:20 -0400

    Add classe Pessoa

---
+++ Pessoa.cs
@@ -0,0 +1,24 @@
+public class Pessoa{
+    private String nome;
+    private int idade;
+    private String cpf;
+    //Construtor da classe Pessoa
+    public Pessoa(string nome, int idade, string cpf){
+        Nome = nome;
+        idade = idade;
+        CPF = cpf; 
+    }
+    public string Nome{
+        get { return nome; }
+        set { nome = value; }
+    }
+    public int Idade{
+        get { return idade; }
+        set { idade = value; }
+    }
+    public string CPF {
+        get { return cpf; }
+        set { cpf = value; }
+    }
+}
+
